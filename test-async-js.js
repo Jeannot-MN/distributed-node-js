2e1c0ce71e7513b793c892baa030235e52c257ee
JeannotMN <jeannot@ssquared.dev>
2022-11-12 12:33:29 +0200

    how well do you understand async js? (Added timing with reference to starting time)

--- test-async-js.js
+++ test-async-js.js
@@ -1,9 +1,10 @@
-setTimeout(() => console.log(new Date().getTime() + '--------->A'), 0);
-console.log(new Date().getTime() + '--------->B');
-setTimeout(() => console.log(new Date().getTime() + '--------->C'), 100);
-setTimeout(() => console.log(new Date().getTime() + '--------->D'), 0);
+const startingTime = new Date().getTime();
+setTimeout(() => console.log((new Date().getTime() - startingTime) + '--------->A'), 0);
+console.log((new Date().getTime() - startingTime) + '--------->B');
+setTimeout(() => console.log((new Date().getTime() - startingTime) + '--------->C'), 100);
+setTimeout(() => console.log((new Date().getTime() - startingTime) + '--------->D'), 0);
 wait(500);
-console.log(new Date().getTime() + '--------->E');
+console.log((new Date().getTime() - startingTime) + '--------->E');
 
 
 function wait(timeInMillis) {
