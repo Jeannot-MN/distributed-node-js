4c6e4b215a1caebf684cf7b2916c827613b56fae
JeannotMN <jeannot@ssquared.dev>
2022-11-18 19:53:13 +0200

    how well do you know node event loop phases

---
+++ event-loop-phases.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+console.log("POLL STAGE");
+
+setImmediate(() => {
+    console.log("CHECK STAGE");
+});
+
+setTimeout(() => {
+    console.log("TIMERS STAGE");
+}, 0);
+
+Promise.resolve().then(() => {
+    console.log("PROMISE MICROTASK QUEUE");
+});
+
+process.nextTick(() => {
+    console.log("NEXT TICK QUEUE");
+});
+
+fs.readFile("test.txt", (data) => {
+    console.log("PENDING CALLBACK");
+
+    setTimeout(() => {
+        console.log("TIMERS STAGE NEXT ITERATION");
+    }, 0);
+
+    setImmediate(() => {
+        console.log("CHECK STAGE NEXT ITERATION");
+    });
+
+    process.nextTick(() => {
+        console.log("THIS IS JUST AFTER THE PENDING CALLBACK");
+    });
+});
+
+console.log("Last Statement");
