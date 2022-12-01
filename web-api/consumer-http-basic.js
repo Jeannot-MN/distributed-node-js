a1bbf3179494aaaeb54a89f58aaf4a99b675cc12
JeannotMN <jeannot@ssquared.dev>
2022-12-01 22:09:31 +0200

    node event loop phases

--- web-api/consumer-http-basic.js
+++ web-api/consumer-http-basic.js
@@ -5,6 +5,7 @@ const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 3000;
 const RECIPE_API_ENDPOINT = process.env.RECIPE_API_ENDPOINT || 'localhost:4000';
 
+
 server.get('/', async() => {
     const response = await fetch(`http://${RECIPE_API_ENDPOINT}/recipes/42`);
     const producer_data = await response.json();
